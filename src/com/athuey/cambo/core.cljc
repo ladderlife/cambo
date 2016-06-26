0c6c9a48342da9cec20bdbbe9b66dcd1ca43a708
Huey Petersen <eyston@gmail.com>
2016-06-26 13:49:10 -0700

    move over core / path algorithms

--- src/com/athuey/cambo/core.cljc
+++ src/com/athuey/cambo/core.cljc
@@ -1,1 +1,184 @@
-(ns com.athuey.cambo.core)
+(ns com.athuey.cambo.core
+  (:refer-clojure :exclude [get set atom ref keys range]))
+
+;;; DATASOURCE
+
+(defprotocol IDataSource
+  (get [this pathsets])
+  (set [this pathmaps]))
+
+;;; GRAPH
+
+(defrecord Atom [])
+
+;; TODO: remember significance of empty atom vs nil value atom ;p
+(defn atom
+  ([]
+   (Atom.))
+  ([value]
+   (map->Atom {:value value})))
+
+(defn atom? [x]
+  (instance? Atom x))
+
+(defrecord Ref [path])
+
+(defn ref [path]
+  (Ref. path))
+
+(defn ref? [x]
+  (instance? Ref x))
+
+(defn boxed?
+  [value]
+  (or (atom? value)
+      (ref? value)))
+
+;; TODO:
+;; - pathmap
+;; - pathvalue
+
+;;; KEYS
+
+(defn key? [x]
+  (or (string? x)
+      (keyword? x)
+      (integer? x)))
+
+(defrecord Range [start end])
+
+(defn range [start end]
+  (Range. start end))
+
+(defn range-keys [{:keys [start end]}]
+  (clojure.core/range start end))
+
+(defn range? [x]
+  (instance? Range x))
+
+;; TODO: remove duplicates
+;; TODO: group into ranges?
+(defn keyset [keys]
+  (if (= 1 (count keys))
+    (first keys)
+    (into [] keys)))
+
+(defn keyset? [x]
+  (or (key? x)
+      (range? x)
+      (and (vector? x)
+           (every? #(or (key? %) (range? %)) x))))
+
+(defn keys [x]
+  (cond
+    (key? x) [x]
+    (range? x) (range-keys x)
+    (vector? x) (mapcat keys x)))
+
+(defn keyset-seq [x]
+  (cond
+    (key? x) [x]
+    (range? x) [x]
+    (vector? x) x))
+
+;;; PATHS
+
+(defn path? [x]
+  (and (vector? x)
+       (every? key? x)))
+
+(defn pathset? [x]
+  (and (vector? x)
+       (every? keyset? x)))
+
+(defn expand-pathset
+  [[keyset & pathset]]
+  (if (seq pathset)
+    (for [key (keys keyset)
+          path (expand-pathset pathset)]
+      (into [key] path))
+    (map vector (keys keyset))))
+
+(defn expand-pathsets [pathsets]
+  (mapcat expand-pathset pathsets))
+
+(def leaf ::leaf)
+(defn leaf? [v] (= leaf v))
+
+;; TODO: remember wtf this is doing -- this has to be accidentally quadratic
+(defn pathsets*
+  [tree]
+  (if (leaf? tree)
+    ['()]
+    (->> tree
+         (reduce (fn [m [key sub-tree]]
+                   (let [paths (pathsets* sub-tree)]
+                     (reduce #(update %1 %2 (fnil conj []) key)
+                             m
+                             paths)))
+                 {})
+         (map (fn [[path keys]]
+                (conj path (keyset keys)))))))
+
+;; TODO: kill this once I remember wtf pathsets* is doing
+(defn pathsets [tree]
+  (map vec (pathsets* tree)))
+
+;; TODO: try and make tree stuff not do deep assoc-in -- maybe sort / recurse to minimize deep tree rebuilding
+;; TODO: same as above but with transients
+
+(defn length-tree
+  [pathsets]
+  (->> pathsets
+       (group-by count)
+       (map (fn [[length pathsets]]
+              (loop [[p & ps] (expand-pathsets pathsets) tree {}]
+                (if p
+                  (recur ps (assoc-in tree p leaf))
+                  [length tree]))))
+       (into {})))
+
+(defn length-tree-pathsets
+  [length-tree]
+  (mapcat (comp pathsets second) length-tree))
+
+(defn tree [pathsets]
+  (loop [[p & ps] (expand-pathsets pathsets) tree {}]
+    (if p
+      (recur ps (assoc-in tree p leaf))
+      tree)))
+
+(defn collapse [pathsets]
+  (length-tree-pathsets (length-tree pathsets)))
+
+(defn assert-inner-reference
+  [cache ref]
+  (loop [cache cache [key & rest] ref]
+    (if (ref? cache)
+      (throw (ex-info "inner reference" {:path ref}))
+      (when (seq rest)
+        (recur (clojure.core/get cache key) rest)))))
+
+;; TODO: redo this ...
+(defn optimize* [root cache [key & rest :as path] optimized]
+  (cond
+    (ref? cache) (let [ref-path (:path cache)]
+                   (assert-inner-reference root ref-path)
+                   (if (seq? path)
+                     (optimize* root root (concat ref-path path) [])
+                     [ref-path]))
+
+    (atom? cache) []
+    :else (mapcat (fn [key]
+                    (if (map? cache)
+                      (if (contains? cache key)
+                        (optimize* root (clojure.core/get cache key) rest (conj optimized key))
+                        [(into [] (concat optimized [key] rest))])
+                      []))
+                  (keys key))))
+
+(defn optimize
+  [cache paths]
+  (mapcat (fn [path]
+            (optimize* cache cache path []))
+          paths))
--- test/com/athuey/cambo/core_test.clj
+++ test/com/athuey/cambo/core_test.clj
@@ -1,1 +1,194 @@
-(ns com.athuey.cambo.core-test)
+(ns com.athuey.cambo.core-test
+  (:refer-clojure :exclude [get set atom ref keys range])
+  (:require [com.athuey.cambo.core :refer :all]
+            [clojure.test :refer :all]))
+
+
+;;; KEYS
+
+(deftest range-keys-test
+         (is (= [4 5 6 7 8 9]
+                (range-keys (range 4 10))))
+         (is (= [4]
+                (range-keys (range 4 5))))
+         (is (= []
+                (range-keys (range 4 4))))
+         (is (= []
+                (range-keys (range 4 3)))))
+
+(deftest keys-test
+  (is (= [:foo]
+         (keys :foo)))
+  (is (= [0 1 :foo]
+         (keys [0 1 :foo])))
+  (is (= [0 1 2 3 4 5]
+         (keys (range 0 6)))))
+
+(deftest keyset-seq-test
+  (is (= [:foo]
+         (keyset-seq :foo)))
+  (is (= [(range 0 5)]
+         (keyset-seq (range 0 5))))
+  (is (= [:foo "bar" 13]
+         (keyset-seq [:foo "bar" 13])))
+  (is (= [:foo "bar" (range 0 5) 13]
+         (keyset-seq [:foo "bar" (range 0 5) 13]))))
+
+(deftest keyset-test
+  (is (= :foo
+         (keyset [:foo])))
+  (is (= [:foo "bar" 13]
+         (keyset [:foo "bar" 13]))))
+
+;;; PATHS
+
+;; TODO: a normalize pathset, etc for keyset comparison (make everything a set for order-independent comparison)
+
+(deftest expand-paths-test
+  (is (= [[:user 0 :name]
+          [:user 0 :age]
+          [:user 1 :name]
+          [:user 1 :age]]
+         (expand-pathsets [[:user (range 0 2) [:name :age]]])))
+  (is (= [[:user 0 :name]
+          [:user 0 :age]
+          [:user 1 :name]
+          [:user 1 :age]
+          [:video 0 :title]
+          [:video 5 :title]
+          [:video 6 :title]
+          [:video 7 :title]]
+         (expand-pathsets [[:user (range 0 2) [:name :age]]
+                           [:video [0 (range 5 8)] :title]]))))
+
+(deftest pathsets-test
+  (is (= [[:one [:two :three]]
+          [:one "four" [0 1]]]
+         (pathsets {:one {:two leaf
+                          :three leaf
+                          "four" {0 leaf
+                                  1 leaf}}}))))
+
+(deftest length-tree-pathsets-test
+  (testing "simple path"
+    (is (= [[:one :two]]
+           (length-tree-pathsets {2 {:one {:two leaf}}}))))
+  (testing "complex path"
+    (is (= [[:one [:two :three]]]
+           (length-tree-pathsets {2 {:one {:two leaf
+                                           :three leaf}}}))))
+  (testing "simple and complex path"
+    (is (= [[:one [:two :three]]
+            [:one [0 1 2 3] :summary]]
+           (length-tree-pathsets {2 {:one {:two leaf
+                                           :three leaf}}
+                                  3 {:one {0 {:summary leaf}
+                                           1 {:summary leaf}
+                                           2 {:summary leaf}
+                                           3 {:summary leaf}}}}))))
+  (testing "pathmap that has overlapping branch and leaf nodes"
+    (is (= [[:lolomo]
+            [:lolomo [:summary 13 14]]
+            [:lolomo [15 16 17] [:rating :summary]]
+            [:lolomo [13 14] :summary]]
+           (length-tree-pathsets {1 {:lolomo leaf}
+                                  2 {:lolomo {:summary leaf
+                                              13 leaf
+                                              14 leaf}}
+                                  3 {:lolomo {15 {:rating leaf
+                                                  :summary leaf}
+                                              13 {:summary leaf}
+                                              16 {:rating leaf
+                                                  :summary leaf}
+                                              14 {:summary leaf}
+                                              17 {:rating leaf
+                                                  :summary leaf}}}})))))
+
+(deftest tree-test
+  (testing "simple path"
+    (is (= {:one {:two leaf}}
+           (tree [[:one :two]]))))
+  (testing "complex path"
+    (is (= {:one {:two leaf
+                  :three leaf}}
+           (tree [[:one [:two :three]]]))))
+  (testing "set of complex and simple paths"
+    (is (= {:one {:two leaf
+                  :three leaf
+                  0 {:summary leaf}
+                  1 {:summary leaf}
+                  2 {:summary leaf}
+                  3 {:summary leaf}}}
+           (tree [[:one [:two :three]]
+                  [:one (range 0 4) :summary]])))))
+
+(deftest collapse-test
+  (is (= [[:genres 0 :titles [0 1] [:name :rating]]]
+         (collapse [[:genres 0 :titles 0 :name]
+                    [:genres 0 :titles 0 :rating]
+                    [:genres 0 :titles 1 :name]
+                    [:genres 0 :titles 1 :rating]])))
+  (is (= [[:genres 0 :titles [0 1] [:name :rating]]]
+         (collapse [[:genres 0 :titles 0 [:name :rating]]
+                    [:genres 0 :titles 1 :name]
+                    [:genres 0 :titles 1 :rating]]))))
+
+(deftest optimize-test
+  (let [cache {:videos-list {3 (ref [:videos 956])
+                             5 (ref [:videos 5])
+                             :double (ref [:videos-list 3])
+                             :short (ref [:videos 5 :more-keys])
+                             :inner (ref [:videos-list 3 :inner])}
+               :videos {5 (atom "title")
+                        6 "a"
+                        7 1
+                        8 true
+                        9 nil}
+               :falsey {:string ""
+                        :number 0
+                        :boolean false
+                        :nil nil}}]
+    (testing "simple path"
+      (is (= [[:videos 956 :summary]]
+             (optimize cache [[:videos-list 3 :summary]]))))
+    (testing "complex path"
+      (is (= [[:videos-list 0 :summary]
+              [:videos 956 :summary]]
+             (optimize cache [[:videos-list [0 3] :summary]]))))
+    (testing "remove found paths"
+      (is (= [[:videos-list 0 :summary]
+              [:videos 956 :summary]]
+             (optimize cache [[:videos-list [0 3 5] :summary]]))))
+    (testing "follow double reference"
+      (is (= [[:videos 956 :summary]]
+             (optimize cache [[:videos-list :double :summary]]))))
+    (testing "short circuit ref"
+      (is (= []
+             (optimize cache [[:videos-list :short :summary]]))))
+    (testing "short circuit string"
+      (is (= []
+             (optimize cache [[:videos 6 :summary]]))))
+    (testing "short circuit number"
+      (is (= []
+             (optimize cache [[:videos 7 :summary]]))))
+    (testing "short circuit boolean"
+      (is (= []
+             (optimize cache [[:videos 8 :summary]]))))
+    (testing "short circuit nil"
+      (is (= []
+             (optimize cache [[:videos 9 :summary]]))))
+    (testing "falsey string not missing"
+      (is (= []
+             (optimize cache [[:falsey :string]]))))
+    (testing "falsey number not missing"
+      (is (= []
+             (optimize cache [[:falsey :number]]))))
+    (testing "falsey boolean not missing"
+      (is (= []
+             (optimize cache [[:falsey :boolean]]))))
+    (testing "falsey nil not missing"
+      (is (= []
+             (optimize cache [[:falsey :nil]]))))
+    (testing "inner reference"
+      (is (thrown? Exception
+                   (optimize cache [[:videos-list :inner :summary]]))))))
